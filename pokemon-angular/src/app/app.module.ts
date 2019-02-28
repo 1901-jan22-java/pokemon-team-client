2c4c06522462242181a19be703750a3f6f1a67f4
Joshua Vanderzee <joshuavanderzee@gmail.com>
2019-02-28 12:50:41 -0500

    login service (does not right now)

--- pokemon-angular/src/app/app.module.ts
+++ pokemon-angular/src/app/app.module.ts
@@ -1,11 +1,17 @@
+// General imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+// Componement imports
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+
+// Services imports
+import { LoginService } from './services/login.service';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +24,9 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    LoginService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
